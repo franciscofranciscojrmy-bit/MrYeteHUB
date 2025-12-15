5c988b246b38285167315024bea141b81d314ba3
Mr Yete <franciscofranciscojrmy@gmail.com>
2025-12-15 14:32:11 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,11 @@
--- MR_Yete Loader
+-- MR_Yete HUB Loader (estable)
 
-loadstring(game:HttpGet(
-	"https://raw.githubusercontent.com/franciscofranciscojrmy-bit/MrYeteHUB/main/main.lua"
-))()
+local url = "https://raw.githubusercontent.com/franciscofranciscojrmy-bit/MrYeteHUB/main/main.lua"
+
+local ok, err = pcall(function()
+	loadstring(game:HttpGet(url))()
+end)
+
+if not ok then
+	warn("[MR_Yete HUB] Error cargando main.lua:", err)
+end
